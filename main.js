b9cca330f53f9c0fb6bde926b86b8074ff70b96d
SamadjonIslomov <149060290+SamadjonIslomov@users.noreply.github.com>
2023-11-05 08:54:40 +0500

    yangi gap qoshildi

--- main.js
+++ main.js
@@ -5,3 +5,6 @@ console.log(`Siz ${age} yilda tug'ilgansz`)
 let miqdor = prompt("Oylik ish haqqingizni kiriting")
 miqdor = miqdor / 12
 console.log(`Siz Davlatga ${miqdor} so'm miqdorida soliq berishingiz kerak`)
+
+
+miqdor = 500
