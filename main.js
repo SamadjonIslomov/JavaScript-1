83881a608474ee599e58bec0ff111a250678ec4d
SamadjonIslomov <149060290+SamadjonIslomov@users.noreply.github.com>
2023-11-05 08:49:39 +0500

    summa qo'shildi

--- main.js
+++ main.js
@@ -3,5 +3,5 @@ age = 2023 - age
 console.log(`Siz ${age} yilda tug'ilgansz`)
 
 let miqdor = prompt("Oylik ish haqqingizni kiriting")
-miqdor = miqdor % 12
-console.log(`Siz Davlatga ${miqdor} shuncha miqdorida soliq berishingiz kerak`)
+miqdor = miqdor / 12
+console.log(`Siz Davlatga ${miqdor} so'm miqdorida soliq berishingiz kerak`)
