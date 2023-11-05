a5ea7dcf1c75f12007bc77bd6e3bd19abf8ffcc2
SamadjonIslomov <149060290+SamadjonIslomov@users.noreply.github.com>
2023-11-05 08:17:17 +0500

    Delete main.js

--- main.js
+++
@@ -1,6 +0,0 @@
-let age = prompt("Yoshingizni kiriting")
-age = 2023 - age
-console.log(`Siz ${age} yilda tug'ilgansz`)
-
-let miqdor = prompt("Oylik ish haqqingizni kiriting")
-miqdor = miqdor % 12
