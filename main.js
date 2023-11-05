fa158ebbe5fc95c5674540a90a83e7cbb76480cd
SamadjonIslomov <149060290+SamadjonIslomov@users.noreply.github.com>
2023-11-05 08:18:17 +0500

    So'zlar qo'shildi

---
+++ main.js
@@ -0,0 +1,6 @@
+let age = prompt("Yoshingizni kiriting")
+age = 2023 - age
+console.log(`Siz ${age} yilda tug'ilgansz`)
+
+let miqdor = prompt("Oylik ish haqqingizni kiriting")
+miqdor = miqdor % 12
